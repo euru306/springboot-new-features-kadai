faac7066703b80eff1d2b5495f045e5a876f456a
euru306 <grado.dragon83@ezweb.ne.jp>
2025-07-09 21:23:03 +0900

    Delete samuraitravel directory

--- samuraitravel/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51RX1bvCv427D6dK1M5DoW6FituV6iEjJc1VHoei8J4tXdqu89CXJLEN72DoaXFXuJzjig4AJ5ZiGynL56Nodvcha00gYgczh9H');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
